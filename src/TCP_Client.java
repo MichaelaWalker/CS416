81f954c6587f0e56b01b8baa110b87283966ee8a
Megan237 <megan.stinefield@bsu.edu>
2023-02-13 10:32:55 -0500

    created way for client to enter what they want to do and send it to the server

--- src/TCP_Client.java
+++ src/TCP_Client.java
@@ -15,13 +15,34 @@ public class TCP_Client {
         String serverIP = args[0];
         int serverPort = Integer.parseInt(args[1]);
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a Message");
+        System.out.println("Enter DL to download, UL to upload, DEL to delete, or RE to rename");
+        String input = scanner.nextLine();
+        String inputToSend = "";
+        if (input.equals("DL")){
+            System.out.println("Enter file name with extension that you want to delete");
+            inputToSend = scanner.nextLine();
+        }
+        if (input.equals("UL")){
+            System.out.println("Enter file name with extension that you want to upload");
+            inputToSend = scanner.nextLine();
+        }
+        if (input.equals("DEL")){
+            System.out.println("Enter the file name with extension you want to delete");
+            inputToSend = scanner.nextLine();
+        }
+        if (input.equals("RE")){
+            System.out.println("Enter the name of the file you want to rename");
+            String originalName = scanner.nextLine();
+            System.out.println("Enter the new name of the file");
+            String newName = scanner.nextLine();
+            inputToSend = originalName + " " + newName;
+        }
+        else {
+            System.out.println("That is not a valid input");
+        }
+        inputToSend += " " + input;
 
-
-
-
-        String message = scanner.nextLine();
-        ByteBuffer buffer = ByteBuffer.wrap(message.getBytes(StandardCharsets.UTF_8));
+        ByteBuffer buffer = ByteBuffer.wrap(inputToSend.getBytes(StandardCharsets.UTF_8));
         SocketChannel sc = SocketChannel.open();
         sc.connect(new InetSocketAddress(serverIP, serverPort));
         buffer.rewind();
